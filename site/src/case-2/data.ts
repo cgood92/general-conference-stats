d11de91dadba6c3e408a2d359cc5fcbc865921da
Clint Goodman <clintg@adobe.com>
2023-01-30 19:41:44 -0700

    feat: building compressed version of data for the web

--- site/src/case-2/data.ts
+++ site/src/case-2/data.ts
@@ -1,217 +1,24 @@
-import talks_1970_04 from "@root/case2/output/1970-04.json";
-import talks_1970_10 from "@root/case2/output/1970-10.json";
-import talks_1971_04 from "@root/case2/output/1971-04.json";
-import talks_1971_10 from "@root/case2/output/1971-10.json";
-import talks_1972_04 from "@root/case2/output/1972-04.json";
-import talks_1972_10 from "@root/case2/output/1972-10.json";
-import talks_1973_04 from "@root/case2/output/1973-04.json";
-import talks_1973_10 from "@root/case2/output/1973-10.json";
-import talks_1974_04 from "@root/case2/output/1974-04.json";
-import talks_1974_10 from "@root/case2/output/1974-10.json";
-import talks_1975_04 from "@root/case2/output/1975-04.json";
-import talks_1975_10 from "@root/case2/output/1975-10.json";
-import talks_1976_04 from "@root/case2/output/1976-04.json";
-import talks_1976_10 from "@root/case2/output/1976-10.json";
-import talks_1977_04 from "@root/case2/output/1977-04.json";
-import talks_1977_10 from "@root/case2/output/1977-10.json";
-import talks_1978_04 from "@root/case2/output/1978-04.json";
-import talks_1978_10 from "@root/case2/output/1978-10.json";
-import talks_1979_04 from "@root/case2/output/1979-04.json";
-import talks_1979_10 from "@root/case2/output/1979-10.json";
-import talks_1980_04 from "@root/case2/output/1980-04.json";
-import talks_1980_10 from "@root/case2/output/1980-10.json";
-import talks_1981_04 from "@root/case2/output/1981-04.json";
-import talks_1981_10 from "@root/case2/output/1981-10.json";
-import talks_1982_04 from "@root/case2/output/1982-04.json";
-import talks_1982_10 from "@root/case2/output/1982-10.json";
-import talks_1983_04 from "@root/case2/output/1983-04.json";
-import talks_1983_10 from "@root/case2/output/1983-10.json";
-import talks_1984_04 from "@root/case2/output/1984-04.json";
-import talks_1984_10 from "@root/case2/output/1984-10.json";
-import talks_1985_04 from "@root/case2/output/1985-04.json";
-import talks_1985_10 from "@root/case2/output/1985-10.json";
-import talks_1986_04 from "@root/case2/output/1986-04.json";
-import talks_1986_10 from "@root/case2/output/1986-10.json";
-import talks_1987_04 from "@root/case2/output/1987-04.json";
-import talks_1987_10 from "@root/case2/output/1987-10.json";
-import talks_1988_04 from "@root/case2/output/1988-04.json";
-import talks_1988_10 from "@root/case2/output/1988-10.json";
-import talks_1989_04 from "@root/case2/output/1989-04.json";
-import talks_1989_10 from "@root/case2/output/1989-10.json";
-import talks_1990_04 from "@root/case2/output/1990-04.json";
-import talks_1990_10 from "@root/case2/output/1990-10.json";
-import talks_1991_04 from "@root/case2/output/1991-04.json";
-import talks_1991_10 from "@root/case2/output/1991-10.json";
-import talks_1992_04 from "@root/case2/output/1992-04.json";
-import talks_1992_10 from "@root/case2/output/1992-10.json";
-import talks_1993_04 from "@root/case2/output/1993-04.json";
-import talks_1993_10 from "@root/case2/output/1993-10.json";
-import talks_1994_04 from "@root/case2/output/1994-04.json";
-import talks_1994_10 from "@root/case2/output/1994-10.json";
-import talks_1995_04 from "@root/case2/output/1995-04.json";
-import talks_1995_10 from "@root/case2/output/1995-10.json";
-import talks_1996_04 from "@root/case2/output/1996-04.json";
-import talks_1996_10 from "@root/case2/output/1996-10.json";
-import talks_1997_04 from "@root/case2/output/1997-04.json";
-import talks_1997_10 from "@root/case2/output/1997-10.json";
-import talks_1998_04 from "@root/case2/output/1998-04.json";
-import talks_1998_10 from "@root/case2/output/1998-10.json";
-import talks_1999_04 from "@root/case2/output/1999-04.json";
-import talks_1999_10 from "@root/case2/output/1999-10.json";
-import talks_2000_04 from "@root/case2/output/2000-04.json";
-import talks_2000_10 from "@root/case2/output/2000-10.json";
-import talks_2001_04 from "@root/case2/output/2001-04.json";
-import talks_2001_10 from "@root/case2/output/2001-10.json";
-import talks_2002_04 from "@root/case2/output/2002-04.json";
-import talks_2002_10 from "@root/case2/output/2002-10.json";
-import talks_2003_04 from "@root/case2/output/2003-04.json";
-import talks_2003_10 from "@root/case2/output/2003-10.json";
-import talks_2004_04 from "@root/case2/output/2004-04.json";
-import talks_2004_10 from "@root/case2/output/2004-10.json";
-import talks_2005_04 from "@root/case2/output/2005-04.json";
-import talks_2005_10 from "@root/case2/output/2005-10.json";
-import talks_2006_04 from "@root/case2/output/2006-04.json";
-import talks_2006_10 from "@root/case2/output/2006-10.json";
-import talks_2007_04 from "@root/case2/output/2007-04.json";
-import talks_2007_10 from "@root/case2/output/2007-10.json";
-import talks_2008_04 from "@root/case2/output/2008-04.json";
-import talks_2008_10 from "@root/case2/output/2008-10.json";
-import talks_2009_04 from "@root/case2/output/2009-04.json";
-import talks_2009_10 from "@root/case2/output/2009-10.json";
-import talks_2010_04 from "@root/case2/output/2010-04.json";
-import talks_2010_10 from "@root/case2/output/2010-10.json";
-import talks_2011_04 from "@root/case2/output/2011-04.json";
-import talks_2011_10 from "@root/case2/output/2011-10.json";
-import talks_2012_04 from "@root/case2/output/2012-04.json";
-import talks_2012_10 from "@root/case2/output/2012-10.json";
-import talks_2013_04 from "@root/case2/output/2013-04.json";
-import talks_2013_10 from "@root/case2/output/2013-10.json";
-import talks_2014_04 from "@root/case2/output/2014-04.json";
-import talks_2014_10 from "@root/case2/output/2014-10.json";
-import talks_2015_04 from "@root/case2/output/2015-04.json";
-import talks_2015_10 from "@root/case2/output/2015-10.json";
-import talks_2016_04 from "@root/case2/output/2016-04.json";
-import talks_2016_10 from "@root/case2/output/2016-10.json";
-import talks_2017_04 from "@root/case2/output/2017-04.json";
-import talks_2017_10 from "@root/case2/output/2017-10.json";
-import talks_2018_04 from "@root/case2/output/2018-04.json";
-import talks_2018_10 from "@root/case2/output/2018-10.json";
-import talks_2019_04 from "@root/case2/output/2019-04.json";
-import talks_2019_10 from "@root/case2/output/2019-10.json";
-import talks_2020_04 from "@root/case2/output/2020-04.json";
-import talks_2020_10 from "@root/case2/output/2020-10.json";
-import talks_2021_04 from "@root/case2/output/2021-04.json";
-import talks_2021_10 from "@root/case2/output/2021-10.json";
-import talks_2022_04 from "@root/case2/output/2022-04.json";
-import talks_2022_10 from "@root/case2/output/2022-10.json";
+import webCompressed from "@root/case2/output/web.json";
 
-const talksCombined = [
-  talks_1970_04,
-  talks_1970_10,
-  talks_1971_04,
-  talks_1971_10,
-  talks_1972_04,
-  talks_1972_10,
-  talks_1973_04,
-  talks_1973_10,
-  talks_1974_04,
-  talks_1974_10,
-  talks_1975_04,
-  talks_1975_10,
-  talks_1976_04,
-  talks_1976_10,
-  talks_1977_04,
-  talks_1977_10,
-  talks_1978_04,
-  talks_1978_10,
-  talks_1979_04,
-  talks_1979_10,
-  talks_1980_04,
-  talks_1980_10,
-  talks_1981_04,
-  talks_1981_10,
-  talks_1982_04,
-  talks_1982_10,
-  talks_1983_04,
-  talks_1983_10,
-  talks_1984_04,
-  talks_1984_10,
-  talks_1985_04,
-  talks_1985_10,
-  talks_1986_04,
-  talks_1986_10,
-  talks_1987_04,
-  talks_1987_10,
-  talks_1988_04,
-  talks_1988_10,
-  talks_1989_04,
-  talks_1989_10,
-  talks_1990_04,
-  talks_1990_10,
-  talks_1991_04,
-  talks_1991_10,
-  talks_1992_04,
-  talks_1992_10,
-  talks_1993_04,
-  talks_1993_10,
-  talks_1994_04,
-  talks_1994_10,
-  talks_1995_04,
-  talks_1995_10,
-  talks_1996_04,
-  talks_1996_10,
-  talks_1997_04,
-  talks_1997_10,
-  talks_1998_04,
-  talks_1998_10,
-  talks_1999_04,
-  talks_1999_10,
-  talks_2000_04,
-  talks_2000_10,
-  talks_2001_04,
-  talks_2001_10,
-  talks_2002_04,
-  talks_2002_10,
-  talks_2003_04,
-  talks_2003_10,
-  talks_2004_04,
-  talks_2004_10,
-  talks_2005_04,
-  talks_2005_10,
-  talks_2006_04,
-  talks_2006_10,
-  talks_2007_04,
-  talks_2007_10,
-  talks_2008_04,
-  talks_2008_10,
-  talks_2009_04,
-  talks_2009_10,
-  talks_2010_04,
-  talks_2010_10,
-  talks_2011_04,
-  talks_2011_10,
-  talks_2012_04,
-  talks_2012_10,
-  talks_2013_04,
-  talks_2013_10,
-  talks_2014_04,
-  talks_2014_10,
-  talks_2015_04,
-  talks_2015_10,
-  talks_2016_04,
-  talks_2016_10,
-  talks_2017_04,
-  talks_2017_10,
-  talks_2018_04,
-  talks_2018_10,
-  talks_2019_04,
-  talks_2019_10,
-  talks_2020_04,
-  talks_2020_10,
-  talks_2021_04,
-  talks_2021_10,
-  talks_2022_04,
-  talks_2022_10,
-].flat();
+const { talks, speakers } = webCompressed as any;
 
-export default talksCombined;
+type Talk = {
+  speaker: string;
+  title: string;
+  talk: string;
+  url: string;
+  year: number;
+  month: number;
+  content: string;
+};
+
+const output: Talk[] = talks.map((talk: any) => ({
+  speaker: speakers[talk.s],
+  title: talk.t,
+  url: `https://www.churchofjesuschrist.org/study/general-conference/${talk.y}/${talk.m}/${talk.u}`,
+  month: talk.m,
+  year: talk.y,
+  content: talk.c,
+}));
+
+export default output;
